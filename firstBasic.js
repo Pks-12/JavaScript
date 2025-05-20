6b026fd172dddcbbdb1ca1094360f716d46aa2c4
Pks-12 <singhpk120901@gmail.com>
2025-05-20 11:07:28 +0530

    First Baisc code of javaScript

---
+++ firstBasic.js
@@ -0,0 +1,16 @@
+const accountId = 12243;
+let accountEmail = "temp@gmail.com";
+var accountPassword = 122345;
+accountCity = "Haridwar"
+
+//accountId = 2356;
+
+//console.log(accountId);
+/*Prefer not to use var
+Because of issue in block scope and function scope */
+accountEmail = "geu.ac.in";
+accountPassword = 1111122;
+accountCity = "Dehradun";
+
+console.log([accountId,accountEmail,accountPassword,accountCity,]);
+
