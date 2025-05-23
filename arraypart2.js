7491890b6a85580ceae7af515beb2520f82f9243
Pks-12 <singhpk120901@gmail.com>
2025-05-23 10:38:41 +0530

    I learn about the array part which contains the of,push,flat,infinity,spread operator

---
+++ arraypart2.js
@@ -0,0 +1,40 @@
+const hero1 = ["A","B","C","D"];
+const hero2 = ["E","F","G","H","I"];
+
+hero1.push(hero2);
+console.log(hero1);
+console.log(hero1[3][1]);
+console.log("Here i see the result");
+
+const allHero = hero1.concat(hero2)
+console.log(allHero);
+
+const all_new_hero = [...hero1, ...hero2];// spread operator 
+
+console.log(all_new_hero);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[7,8]]];
+const real_another_array = another_array.flat(Infinity);// if we calculated or find the array, if they use multiple array of them. we use the flat method.
+console.log(real_another_array);
+
+console.log(Array.isArray("RHCSA"));
+console.log(Array.from("RHCSA"));
+console.log(Array.from({name:"ramesh"}));
+
+let score1 = 300;
+let score2 = 330;
+let score3 = 400;
+
+console.log(Array.of(score1,score2,score3));
+
+const newArray = [1,2,"A","B",["ramesh","ravi","yash"],[20,30,40],"isLoggedIn", "true"];// this variable store multiple array inside them.
+
+console.log(newArray);
+
+
+
+
+
+
+
+
