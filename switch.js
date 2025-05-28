5f5ae66bf84c1b3a246baf3870d99592bc2afc96
Pks-12 <singhpk120901@gmail.com>
2025-05-28 11:19:37 +0530

    Controls flow and for loop

---
+++ switch.js
@@ -0,0 +1,50 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+              
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+const month = "September";
+switch (month) {
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("march");
+        break;
+    case "April":
+        console.log("April");
+        break;
+              
+    default:
+        console.log("default case match");
+        
+        break;
+}
