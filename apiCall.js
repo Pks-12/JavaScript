b735b77f29bd6c7718db1724c59cb5190f1a93a6
Pks-12 <singhpk120901@gmail.com>
2025-06-06 11:43:29 +0530

    Add more api to practice them

--- apiCall.js
+++ apiCall.js
@@ -1,9 +1,51 @@
-fetch('https://api.github.com/users/UserID')
-.then((response)=>{
-    return response.json()
+// fetch('https://fakerestapi.azurewebsites.net/api/v1/Books')
+// .then((response)=>{
+//     return response.json()
+// })
+// .then((data)=>{
+//     data.forEach(book=>{
+//         console.log(data.title);
+//         console.log(data.id);
+        
+//     });
+// })
+// .catch((error)=>console.log(error)
+// )
+
+// fetch('https://fakerestapi.azurewebsites.net/api/v1/Books')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // Loop through all books and log each title
+//     const totalID = data.length;
+//     console.log(`Total length of book${totalID}`);
+    
+//     data.forEach(book => {
+//       console.log(book.id); 
+//       console.log(book.title);
+//       console.log(book.pageCount);
+      
+//       // "title" is correct
+//     });
+//   })
+//   .catch((error) => console.log('Error:', error));
+
+
+
+// fetch("https://fakerestapi.azurewebsites.net/api/v1/CoverPhotos")
+// .then((response)=>{
+//     return response.json()
+// })
+// .then((data)=>{
+//     console.log(data.url);
+// })
+// .catch((error)=>console.log(error)//    i cannot access the value because of this array not object. if this is object we can access the single value.
+// )
+
+fetch("https://fakerestapi.azurewebsites.net/api/v1/CoverPhotos")
+.then((response)=> response.json())
+.then ((data)=>{
+    data.forEach(photo=>{
+        console.log(photo.url);
+        
+    })
 })
-.then((data)=>{
-    console.log(data.id);
-})
-.catch((error)=>console.log(error)
-)
