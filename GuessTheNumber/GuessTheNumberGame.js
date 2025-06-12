1285c0731e2391aada3a7af075e6e559480a4ef0
Pks-12 <singhpk120901@gmail.com>
2025-06-12 11:36:48 +0530

    Update the one if condition

--- GuessTheNumber/GuessTheNumberGame.js
+++ GuessTheNumber/GuessTheNumberGame.js
@@ -2,6 +2,10 @@ const secretNumber = 7;
 let attempts = 3;
 while(attempts>0){
     const guess = parseInt(prompt("Guess the Number 1 to 10"));
+       if (guess < 1 || guess > 10 || isNaN(guess)) {
+        alert("❗ Please enter a number between 1 and 10!");
+        continue; // Skip the rest of the loop and prompt again
+    }
     if(guess === secretNumber){
         alert("You win the game!");
         break;
