68a2a4487210b63bbbaacf279952fc8f09292b14
Pks-12 <singhpk120901@gmail.com>
2025-06-24 16:42:56 +0530

    Improve one if condition search

--- playerObject/api.js
+++ playerObject/api.js
@@ -79,3 +79,54 @@ document.getElementById("search").addEventListener("click", function () {
     })
     .catch(error => console.error("Error loading JSON:", error));
 });
+
+
+// document.getElementById("search").addEventListener("click", function () {
+//   const searchValue = document.getElementById("searchInput").value.trim().toLowerCase();
+
+//   fetch("player.json")
+//     .then(response => response.json())
+//     .then(data => {
+//       const container = document.getElementById("players");
+//       container.innerHTML = ""; // Clear previous results
+
+//       // Only allow exact word 'bowler' or 'bowlers'
+//       if (searchValue === "bowler" || searchValue === "bowlers") {
+//         document.getElementById("heading").textContent = `${data.length} Top Bowlers`;
+  
+//         data.forEach(player => {
+//           const card = document.createElement("div");
+
+//           const img = document.createElement("img");
+//           img.src = player.playerImages.images;
+//           img.alt = player.fullName;
+//           img.width = 200;
+
+//           const name = document.createElement("h3");
+//           name.textContent = player.fullName;
+//           name.style.color = "white";
+
+//           const role = document.createElement("p");
+//           role.textContent = `Role: ${player.personalInformation.roleType}`;
+//           role.style.color = "white";
+
+//           const team = document.createElement("h2");
+//           team.textContent = player.careerInformation.nationalTeam;
+//           team.style.color = "white";
+
+//           card.appendChild(img);
+//           card.appendChild(name);
+//           card.appendChild(role);
+//           card.appendChild(team);
+
+//           container.appendChild(card);
+//         });
+//       } else {
+//         document.getElementById("heading").textContent = "No Results Found";
+//       }
+      
+//     })
+//     .catch(error => console.error("Error loading JSON:", error));
+// });
+
+// if users search the bowler or bowler it only show the result other wise it return nothing.... hahah
